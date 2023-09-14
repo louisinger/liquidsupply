331a7dfb1839df0bbc0b22d56e68dd5879a13f74
Louis Singer <louis@vulpem.com>
2023-09-14 12:20:27 +0200

    add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			entries: [
+				'*',
+				'/liquid/0dea022a8a25abb128b42b0f8e98532bc8bd74f8a77dc81251afcc13168acef7',
+				'/liquid/518c0b351f5731f5d40cf6ad444d1c147eda1cdf8c867185c58a526fb02ad806',
+				'/liquid/ce091c998b83c78bb71a632313ba3760f1763d9cfcffae02258ffa9865a37bd2',
+			]
+		}
 	}
 };
 
